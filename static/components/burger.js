41f604af4ad3565a7d668a3bd75b059e043632ed
Zaynullin Salavat <salavatzaynullin67@gmail.com>
2021-12-17 01:42:02 +0500

    исправил ошибки, добавил данные

--- static/components/burger.js
+++ static/components/burger.js
@@ -1,11 +1,16 @@
-const burger = document.querySelector('.burger');
+const main = document.querySelector('.main');
+const burger = document.querySelector('.header__burger');
 const nav = document.querySelector('.navigation');
 const links = document.querySelectorAll('.navigation__element');
 function slider() {
    burger.addEventListener('click', () => {
        nav.classList.toggle('nav-active');
-       burger.classList.toggle('toggle');    
+       burger.classList.toggle('burger-open');    
    })
+   main.addEventListener('click', () => {
+    nav.classList.remove('nav-active');
+    burger.classList.remove('burger-open');    
+})
 }
 
 slider();
